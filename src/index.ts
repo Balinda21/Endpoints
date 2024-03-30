cb12a47fb04faf9bb84acac453245c20634c696a
Maurice <balindamoris@gmail.com>
2024-03-30 22:13:53 +0200

    changes

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: "https://portfolio-backend-cy9p.onrender.com",
+  origin: ["https://balinda21.github.io/MY-BRAND/","https://portfolio-backend-cy9p.onrender.com"],
   credentials: true
 }));
 
