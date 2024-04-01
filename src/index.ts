23e1ffae4bce3a52ba091988a76183c3724a8d88
Maurice <balindamoris@gmail.com>
2024-04-01 22:31:38 +0200

    changes

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,7 @@ app.use(cookieParser());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://balinda21.github.io');
+  res.setHeader('Access-Control-Allow-Origin', 'https://portfolio-backend-cy9p.onrender.com/submit-contact-form');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
