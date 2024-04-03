4f413910c158abb4ffe316a20aba9cdb8b1c7b06
Maurice <balindamoris@gmail.com>
2024-04-03 09:11:12 +0200

    changes

--- dist/index.js
+++ dist/index.js
@@ -18,8 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://balinda21.github.io/MY-BRAND/');
-    res.setHeader('Access-Control-Allow-Origin', 'https://portfolio-backend-cy9p.onrender.com/api/user/');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
